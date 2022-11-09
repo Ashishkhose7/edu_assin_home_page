46f6ac090f3fa3ada9dc2ee63f17359171d00a33
ashish khose <100353863+Ashishkhose7@users.noreply.github.com>
2022-11-09 18:49:27 +0530

    Add files via upload

---
+++ assign-2/home.js
@@ -0,0 +1,10 @@
+function rating(){
+    var elems = document.querySelectorAll(".star-icon");
+    var index = 0,
+      length = elems.length;
+      elems[0].style.color = "red";
+    // for (; index < length; index++) {
+    //   elems[index].style.color = "red";
+    // }
+    console.log(elems)
+}
