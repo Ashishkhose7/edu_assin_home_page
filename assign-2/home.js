5b2c7e78042716466f16b111f3229a42b332771f
ashish khose <100353863+Ashishkhose7@users.noreply.github.com>
2022-12-10 16:14:09 +0530

    Update home.js

--- assign-2/home.js
+++ assign-2/home.js
@@ -1,10 +1,37 @@
-function rating(){
-    var elems = document.querySelectorAll(".star-icon");
-    var index = 0,
-      length = elems.length;
-      elems[0].style.color = "red";
-    // for (; index < length; index++) {
-    //   elems[index].style.color = "red";
-    // }
-    console.log(elems)
+let loc_url = "https://zomatoapi.onrender.com/location";
+let optLocation = document.getElementById("location")
+fetch(loc_url, { method: "GET" })
+  .then(response => response.json())
+  .then((data) => {
+    data.map((item) => {
+      let add_option = document.createElement("option")
+
+      let add_text = document.createTextNode(item.state)
+      add_option.appendChild(add_text)
+      add_option.value = item.state_id
+      optLocation.appendChild(add_option)
+      // console.log(add_option)
+    })
+  })
+
+
+let getRestaurant = () => {
+  let getCity = document.getElementById("location").value
+  let optRestaurants = document.getElementById("getRestaurant")
+  let res_url = "https://zomatoapi.onrender.com/restaurants";
+  while(optRestaurants.length>0){
+      optRestaurants.remove(optRestaurants.children)
+  }
+  fetch(res_url, { method: "GET" })
+    .then(res => res.json())
+    .then((data) => {
+      data.map((item) => {
+        if(item.state_id==getCity){
+          let add_option = document.createElement("option")
+          let add_text = document.createTextNode(`${item.restaurant_name}, ${item.address}`)
+          add_option.appendChild(add_text)
+          optRestaurants.appendChild(add_option)
+        }
+      })
+    })
 }
