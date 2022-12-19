fe2ab609f3728fbafd3050f6b412ce53f2378a39
ashish khose <100353863+Ashishkhose7@users.noreply.github.com>
2022-12-19 12:27:32 +0530

    Update home.js

--- assign-2/home.js
+++ assign-2/home.js
@@ -1,3 +1,4 @@
+//  Fetch states (dynamic dropdown)
 let loc_url = "https://zomatoapi.onrender.com/location";
 let optLocation = document.getElementById("location")
 fetch(loc_url, { method: "GET" })
@@ -5,28 +6,33 @@ fetch(loc_url, { method: "GET" })
   .then((data) => {
     data.map((item) => {
       let add_option = document.createElement("option")
-
       let add_text = document.createTextNode(item.state)
       add_option.appendChild(add_text)
       add_option.value = item.state_id
       optLocation.appendChild(add_option)
-      // console.log(add_option)
     })
   })
 
-
+// Fetch resaturants from state_id
 let getRestaurant = () => {
   let getCity = document.getElementById("location").value
   let optRestaurants = document.getElementById("getRestaurant")
   let res_url = "https://zomatoapi.onrender.com/restaurants";
-  while(optRestaurants.length>0){
-      optRestaurants.remove(optRestaurants.children)
+
+  while (optRestaurants.length > 0) {
+    optRestaurants.remove(optRestaurants.children)
   }
+
+  let add_option = document.createElement("option")
+  let add_text = document.createTextNode(`Please Select Restaurants`)
+  add_option.appendChild(add_text)
+  optRestaurants.appendChild(add_option)
+
   fetch(res_url, { method: "GET" })
     .then(res => res.json())
     .then((data) => {
       data.map((item) => {
-        if(item.state_id==getCity){
+        if (item.state_id == getCity) {
           let add_option = document.createElement("option")
           let add_text = document.createTextNode(`${item.restaurant_name}, ${item.address}`)
           add_option.appendChild(add_text)
@@ -34,4 +40,16 @@ let getRestaurant = () => {
         }
       })
     })
+}
+
+//Ratings
+let star = document.getElementsByClassName("star-icon")
+for (const key in star) {
+ star[key].onclick = ()=>{
+  if(star[key].style.color=='yellow'){
+    star[key].style.color = "lightyellow"
+  }else{
+    star[key].style.color = "yellow"
+  }
+ }
 }
